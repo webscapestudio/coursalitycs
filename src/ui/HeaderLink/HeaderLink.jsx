f0ed09d869284f6bb1c52500f5411bd45ff2e124
Евгений Дианов <dianov.js@gmail.com>
2022-09-07 21:41:30 +0300

    lskd

--- src/ui/HeaderLink/HeaderLink.jsx
+++ src/ui/HeaderLink/HeaderLink.jsx
@@ -1,13 +1,17 @@
-import cn from "classnames"
-import Link from "next/link"
-import s from "./HeaderLink.module.scss"
+import cn from "classnames";
+import Link from "next/link";
+import s from "./HeaderLink.module.scss";
 
-export const HeaderLink = ({ children, link, className, ...props }) => {
+export const HeaderLink = ({ children, link, className, sub, ...props }) => {
   return (
-    <Link href={link}>
-      <a className={cn(s.link, className)} {...props}>
-        {children}
-      </a>
-    </Link>
-  )
-}
+    <>
+      <Link href={link}>
+        <a className={cn(s.link, className)} {...props}>
+          {children}
+        </a>
+      </Link>
+
+      {sub && <div className={s.drop}>lkj</div>}
+    </>
+  );
+};
