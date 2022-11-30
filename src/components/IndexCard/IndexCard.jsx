12ddd95bb4f021402b73fcdaae7ee2372bd5494d
Your Name <you@example.com>
2022-11-30 19:47:44 +0300

    Date format

--- src/components/IndexCard/IndexCard.jsx
+++ src/components/IndexCard/IndexCard.jsx
@@ -1,23 +1,38 @@
-import s from "./IndexCard.module.scss"
-import HomeIcon from "./home.svg"
-import PinIcon from "./pin.svg"
+import s from "./IndexCard.module.scss";
+import HomeIcon from "./home.svg";
+import PinIcon from "./pin.svg";
 import Link from "next/link";
 
 export const IndexCard = ({ item }) => {
+  const options = {
+    // weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  console.log(
+    new Date(item.occasion.start_at).toLocaleDateString("en-US", options)
+  );
   return (
     <Link href={`https://coursalytics.com/courses/${item.slug}`}>
-    <div className={s.card}>
-      <h3>{item.name}</h3>
-      <div className={s.row}>
-        <HomeIcon />
-        <p className={s.address}>{item.division.title}</p>
+      <div className={s.card}>
+        <h3>{item.name}</h3>
+        <div className={s.row}>
+          <HomeIcon />
+          <p className={s.address}>{item.division.title}</p>
+        </div>
+        <div className={s.row}>
+          <PinIcon />
+          <p className={s.location}>{item.occasion.location}</p>
+        </div>
+        <p className={s.date}>
+          {new Date(item.occasion.start_at).toLocaleDateString(
+            "en-US",
+            options
+          )}
+        </p>
       </div>
-      <div className={s.row}>
-        <PinIcon />
-        <p className={s.location}>{item.occasion.location}</p>
-      </div>
-      <p className={s.date}>{item.occasion.start_at}</p>
-    </div>
     </Link>
-  )
-}
+  );
+};
